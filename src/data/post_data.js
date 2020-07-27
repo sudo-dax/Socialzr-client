eaa78f3633d68201fd94db07b4f677e1726b00f5
sudo-dax <dax.scholtz@gmail.com>
2020-07-27 15:27:56 +1000

    Added to forms, refactored code and fixed eedit function

--- src/data/post_data.js
+++ src/data/post_data.js
@@ -6,7 +6,7 @@ export default [
     modifiedDate: new Date('2019-09-20'),
     category: 'Festival',
     location: 'Melbourne',
-    content:
+    description:
          'Rainbow Serpent Festival has come a long way over the years. From a small gathering in a secluded clearing, to an internationally recognised, multifaceted weekend of dance, colour, expression and celebration. People now travel from all over the world to Western Victoria to experience what is widely regarded as one of the best music, art and lifestyle festivals in the world.'
   },
   {
@@ -15,7 +15,7 @@ export default [
     modifiedDate: new Date('2019-08-30'),
     category: 'Festival',
     location: 'LandCruiser Park, Ipswich',
-    content:
+    description:
          'Earth Frequency Festival is a music, arts, lifestyle and environmental festival based in South-East Queensland, Australia, with a strong focus on arts, education, healing and community spirit. While drawing from many cultural niches such as transformational festival, tribal gathering, doof and boutique festival, the aim of Earth Frequency Festival is to go beyond these traditional tags and to exist as a multi-faceted gathering focused broadly on creativity, connection, intention and inspiration.'
   },
   {
@@ -24,7 +24,7 @@ export default [
     modifiedDate: new Date('2020-02-10'),
     category: 'Festival',
     location: 'Brisbane Show Grounds',
-    content:
+    description:
          'The Big Day Out was an annual music festival that was held in five Australian cities: Sydney, Melbourne, Gold Coast, Adelaide and Perth, as well as Auckland, New Zealand. The festival was held during summer, typically in January of each year but was sometimes held as late as early February in some cities including Perth.'
   },
   {
@@ -33,7 +33,7 @@ export default [
     modifiedDate: new Date('2020-01-30'),
     category: 'Event',
     location: 'Brisbane Museum',
-    content:
+    description:
          'A Human Adventure is a thrilling journey through humankind’s exploration of space. Come face to face with NASA’s remarkable achievements in human space flights and space exploration – the greatest adventure that humans have ever undertaken. Discover the exciting history of rocket science and space flight, and learn about some of the most extraordinary accomplishments in space technology. NASA – A Human Adventure is the most comprehensive and extensive touring space flight exhibition in the world, featuring an extraordinary collection of more than 250 historically significant artefacts from the United States and Soviet Union space programs. The objects on display include original space flown objects, high fidelity replicas and scale models.'
   },
   {
@@ -42,7 +42,7 @@ export default [
     modifiedDate: new Date('2019-12-19'),
     category: 'Party',
     location: 'Brisbane',
-    content:
+    description:
          'Pizza & Pool Party'
   }
 ]
